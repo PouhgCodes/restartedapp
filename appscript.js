041cee00b7d50367add6cfa3c6285f721324285b
PouhgCodes <142100305+PouhgCodes@users.noreply.github.com>
2023-08-15 09:54:21 +0200

    Update appscript.js

--- appscript.js
+++ appscript.js
@@ -1,13 +1,13 @@
-
+// Set constraints for the video stream
 var constraints = { video: true, audio: false, facingMode: "environment" };
 
 // Define constants
 const cameraView = document.querySelector("#camera--view");
 const leftView = document.querySelector("#camera--View").cloneNode(true);
 const rightView = document.querySelector("#camera--View").cloneNode(true);
-    
+
 // Access the device camera and stream to cameraView
-function cameraStart() {
+window.onload = function() {
 navigator.mediaDevices
 .getUserMedia(constraints)
 .then(function(stream) {
@@ -18,9 +18,6 @@ rightView.srcObject = stream;
 .catch(function(error) {
 console.error("Oops. Something is broken.", error);
 });
-}
-    
-// Start the video stream when the window loads 
-window.addEventListener("load", cameraStart, true);
-    
+};
+
 const cameraViews = document.querySelectorAll("#camera--view");
