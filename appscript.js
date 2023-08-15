0f5f3b4815168bd837212c8985b8bdd2f78c29d6
PouhgCodes <142100305+PouhgCodes@users.noreply.github.com>
2023-08-15 09:22:57 +0200

    Add files via upload

---
+++ appscript.js
@@ -0,0 +1,24 @@
+// Set constraints for the video stream
+var constraints = { video: true, audio: false, facingMode: "environment" };
+
+// Define constants
+const cameraView = document.querySelector("#camera--view");
+const leftView = document.querySelector("#camera--View").cloneNode(true);
+const rightView = document.querySelector("#camera--View").cloneNode(true);
+    
+// Access the device camera and stream to cameraView
+function cameraStart() {
+navigator.mediaDevices
+.getUserMedia(constraints)
+.then(function(stream) {
+cameraView.srcObject = stream;
+})
+.catch(function(error) {
+console.error("Oops. Something is broken.", error);
+});
+}
+    
+// Start the video stream when the window loads 
+window.addEventListener("load", cameraStart, true);
+    
+const cameraViews = document.querySelectorAll("#camera--view");
